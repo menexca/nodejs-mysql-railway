1c5f18665d084be9b0d9c50a55ecc9c4443c2941
menexca <36345196+menexca@users.noreply.github.com>
2023-06-01 11:12:21 -0400

    CORREGIDO P.IVA

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import { PORT } from './config.js'
 const app = express()
 
 app.get('/Productos', async (req, res) => {
-  const [rows] = await pool.query(`Select P.CodigoProducto, P.Nombre, (case when p.IVA='A' then round(PP.PrecioMoneda*1.16,2) else PP.PrecioMoneda end) as PrecioMoneda, P.existencia from Productos P left join ProductosPrecios PP on P.CodigoProducto=PP.CodigoProducto where Existencia>0`)
+  const [rows] = await pool.query(`Select P.CodigoProducto, P.Nombre, (case when P.IVA='A' then round(PP.PrecioMoneda*1.16,2) else PP.PrecioMoneda end) as PrecioMoneda, P.existencia from Productos P left join ProductosPrecios PP on P.CodigoProducto=PP.CodigoProducto where Existencia>0`)
   res.json(rows)
 })
 
