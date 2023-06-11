a7476d1537ac540bcc51ff96f8899b50e08dbdad
menexca <luisdaniel_ca@hotmail.com>
2023-06-10 22:12:25 -0400

    agregados solicitud post pedidos

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ import { PORT } from './config.js'
 
 const app = express()
 
+app.use(express.json());
+
+
 app.get('/Productos', async (req, res) => {
   const [rows] = await pool.query(`Select P.CodigoProducto, P.Nombre, (case when P.IVA='A' then round(PP.PrecioMoneda*1.16,2) else PP.PrecioMoneda end) as PrecioMoneda, P.existencia from Productos P left join ProductosPrecios PP on P.CodigoProducto=PP.CodigoProducto where Existencia>0 Order by Nombre`)
   res.json(rows)
@@ -23,6 +26,26 @@ app.get('/Pedidos', async (req, res) => {
   const [rows] = await pool.query('SELECT CodigoCliente, Numero, date(FechaEmision) as FechaEmision, TotalPedido2, Vendedor FROM Pedidos WHERE TotalPedido>0 ORDER BY FechaEmision DESC')
   res.json(rows)
 })
+
+
+app.post('/Pedidos', async (req, res) => {
+  try {
+    const pedido = req.body; // Obtén los datos del pedido enviados en la solicitud POST
+    
+    // Realiza la lógica necesaria para insertar el nuevo pedido en la base de datos utilizando los datos proporcionados en 'pedido'
+    const insertQuery = `INSERT INTO Pedidos (Numero, CodigoCliente, FechaEmision, TotalPedido2, Vendedor) VALUES (?, ?, ?, ?, ?)`;
+    const values = [pedido.Numero, pedido.CodigoCliente, pedido.FechaEmision, pedido.TotalPedido2, pedido.Vendedor];
+    await pool.query(insertQuery, values);
+    
+    // Envía una respuesta indicando que el pedido se ha creado correctamente
+    res.status(201).json({ message: 'Pedido creado exitosamente', numeroPedido });
+  } catch (error) {
+    // Si ocurre un error durante el proceso de creación del pedido, envía una respuesta de error
+    console.error('Error al crear el pedido:', error);
+    res.status(500).json({ error: 'Error al crear el pedido' });
+  }
+});
+
 
 app.get('/ping', async (req, res) => {
   const [result] = await pool.query(`SELECT "hello world" as RESULT`);
