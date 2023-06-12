5e038f9fba05e9f66bdc5cf9fa4b4ce1b4e85b1f
menexca <luisdaniel_ca@hotmail.com>
2023-06-12 06:53:43 -0400

    agregados solicitud post pedidos222

--- src/app.js
+++ src/app.js
@@ -33,8 +33,8 @@ app.post('/Pedidos', async (req, res) => {
     const pedido = req.body; // Obtén los datos del pedido enviados en la solicitud POST
     
     // Realiza la lógica necesaria para insertar el nuevo pedido en la base de datos utilizando los datos proporcionados en 'pedido'
-    const insertQuery = `INSERT INTO Pedidos (Numero, CodigoCliente, FechaEmision, TotalPedido2, Vendedor) VALUES (?, ?, ?, ?, ?)`;
-    const values = [pedido.Numero, pedido.CodigoCliente, pedido.FechaEmision, pedido.TotalPedido2, pedido.Vendedor];
+    const insertQuery = `INSERT INTO Pedidos (Numero, FechaEmision, FechaEntrega, CodigoCliente, TotalBruto, Descuento, Impuesto, Cargo, TotalPedido, PorcentajeDescuento, Vendedor, Comentarios, Tarifa, Almacen, Peso, Estatus, Usuario, Cambio, Moneda, TotalBruto2, Descuento2, Impuesto2, Cargo2, TotalPedido2, Idmoneda) SELECT ?, ?, ?, ?, round(pp.PrecioMoneda*27.02,2)*?, 0, (round(pp.PrecioMoneda*27.02,2)*?)*0.16, 0, (round(pp.PrecioMoneda*27.02,2)*?)*1.16, 0, '012', 'Enviado desde la APP', 'A', '02', round(p.Peso*?,2), 'PE', 'APP', 27.02, 'BsS', pp.PrecioMoneda*?, 0, pp.PrecioMoneda*?*0.16, 0, pp.PrecioMoneda*?*1.16, 2 FROM Productos p left join ProductosPrecios pp on p.CodigoProducto=pp.CodigoProducto where p.CodigoProducto='ZAT01000' and pp.Tarifa='A'`;
+    const values = [pedido.Numero, pedido.FechaEmision, pedido.FechaEmision, pedido.CodigoCliente, pedido.Cantidad];
     await pool.query(insertQuery, values);
     
     // Envía una respuesta indicando que el pedido se ha creado correctamente
