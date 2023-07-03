e55eba24a644ca6d98536d81d6d59dd53f8001ee
menexca <luisdaniel_ca@hotmail.com>
2023-07-02 21:10:36 -0400

    modificado EndPoints de Productos

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 
 app.get('/Productos', async (req, res) => {
-  const [rows] = await pool.query(`Select P.CodigoProducto, P.Nombre, CONCAT(CAST((P.CodigoProducto) AS CHAR),' ',P.Nombre) as NombreBusqueda, P.IVA, P.CantidadxBulto, P.Existencia, P.Existencia02, P.Existencia03, (P.Existencia02 + P.Existencia03) as ExistenciaVenta , (case when P.PedidoVenta is null then 0 else P.PedidoVenta end) as PedidoVenta, PP.PrecioMoneda from Productos P left join ProductosPrecios PP on P.CodigoProducto=PP.CodigoProducto where Visible=1 Order by P.CodigoGrupo, P.Marca, P.Nombre`)
+  const [rows] = await pool.query(`Select P.CodigoProducto, P.Nombre, CONCAT(CAST((P.CodigoProducto) AS CHAR),' ',P.Nombre) as NombreBusqueda, P.IVA, P.CantidadxBulto, P.Existencia, P.Existencia02, P.Existencia03, (P.Existencia02 + P.Existencia03) as ExistenciaVenta , (case when P.PedidoVenta is null then 0 else P.PedidoVenta end) as PedidoVenta, (case when PP.PrecioMoneda is null then 0 else PP.PrecioMoneda end) as PrecioMoneda from Productos P left join ProductosPrecios PP on P.CodigoProducto=PP.CodigoProducto where Visible=1 Order by P.CodigoGrupo, P.Marca, P.Nombre`)
   res.json(rows)
 })
 
